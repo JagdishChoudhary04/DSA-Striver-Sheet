6417a0a9912197f10a333004fac844179a9cd54e
Jagdish Choudhary <jagdishchodhary426@gmail.com>
2025-09-09 19:20:16 +0530

    Pattern 5 Added

---
+++ 2.Patterns/Pattern5.cpp
@@ -0,0 +1,33 @@
+/* 
+Pattern 5
+Given an integer n. You need to recreate the pattern given below for any value of N. 
+Let's say for N = 5, the pattern should look like as below:
+
+*****
+****
+***
+**
+*
+
+Print the pattern in the function given to you.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    static void pattern5(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n-i; j++) cout << "*";
+            cout << endl;
+        }
+    }
+};
+
+int main() {
+    int N = 5;
+    Solution sol;
+    sol.pattern5(N);
+    return 0;
+}
