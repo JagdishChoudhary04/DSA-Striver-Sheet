b0b03593a4e290845353d45dda3aca965582a7d0
Jagdish Choudhary <jagdishchodhary426@gmail.com>
2025-09-16 17:45:07 +0530

    Pattern 12

--- 2.Patterns/Pattern12.cpp
+++ 2.Patterns/Pattern12.cpp
@@ -1,4 +1,44 @@
 /*
 Pattern 12
+Given an integer n. You need to recreate the pattern given below for any value of N. 
+Let's say for N = 5, the pattern should look like as below:
 
+1        1
+12      21
+123    321
+1234  4321
+1234554321
+
+Print the pattern in the function given to you.
 */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void pattern12(int n) {
+      int spaces = 2*(n-1);
+      for(int i=1;i<=n;i++){
+          for(int j=1;j<=i;j++){
+              cout<<j;
+          }
+          for(int j = 1;j<=spaces;j++){
+              cout<<" ";
+          }
+          for(int j=i;j>=1;j--){
+              cout<<j;
+          }
+          cout<<endl;
+          spaces-=2;
+        }
+    }
+
+};
+
+int main() {
+    int N = 5;
+    Solution sol;
+    sol.pattern12(N);
+    return 0;
+}
