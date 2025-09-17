63b0435c70dcaf17f06d0e84fb28ec54ff92b0d4
Jagdish Choudhary <jagdishchodhary426@gmail.com>
2025-09-17 17:33:49 +0530

    Pattern 13 updated

--- 2.Patterns/Pattern13.cpp
+++ 2.Patterns/Pattern13.cpp
@@ -19,15 +19,14 @@ class Solution {
 public:
     void pattern13(int n) {
         int num = 1;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                cout<<num<<" ";
-                num =num+1;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                cout << num <<" ";
+                num = num + 1;
             }
-            cout<<endl;
+            cout << endl;
         }
     }
-
 };
 
 int main() {
