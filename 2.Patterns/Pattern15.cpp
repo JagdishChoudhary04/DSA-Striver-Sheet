ed7f90a8ed582603f45d754bcb75fc1e4e2692c9
Jagdish Choudhary <jagdishchodhary426@gmail.com>
2025-09-19 17:34:19 +0530

    Pattern 15 Added

---
+++ 2.Patterns/Pattern15.cpp
@@ -0,0 +1,34 @@
+/*
+Pattern 15
+Given an integer n. You need to recreate the pattern given below for any value of N. 
+Let's say for N = 5, the pattern should look like as below:
+
+ABCDE
+ABCD
+ABC
+AB
+A
+
+Print the pattern in the function given to you.
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void pattern15(int n) {
+        for(int i = 0; i < n; i++){
+            for(char ch = 'A'; ch <= 'A' + (n-i-1); ch++){
+                cout << ch;
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main() {
+    int N = 5;
+    Solution sol;
+    sol.pattern15(N);
+    return 0;
+}
