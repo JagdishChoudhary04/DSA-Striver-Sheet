7db3e091cb8cc6d0fe4834f1e13fdc17c22414df
Jagdish Choudhary <jagdishchodhary426@gmail.com>
2025-09-22 18:12:54 +0530

    Pattern 18 Added

---
+++ 2.Patterns/Pattern18.cpp
@@ -0,0 +1,35 @@
+/*
+Pattern 18
+Given an integer n. You need to recreate the pattern given below for any value of N. 
+Let's say for N = 5, the pattern should look like as below:
+
+E 
+D E 
+C D E 
+B C D E 
+A B C D E
+
+Print the pattern in the function given to you.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void pattern18(int n) {
+        for(int i = 0; i < n; i++) {
+            for(char ch = ('A'+ n-1)-i; ch <= ('A'+ n-1); ch++){
+                cout<<ch<<" ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main() {
+    int N = 5;
+    Solution sol;
+    sol.pattern18(N);
+    return 0;
+}
